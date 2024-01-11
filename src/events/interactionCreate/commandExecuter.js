267a01ef972ebdf8e30ff6a294a67f5b63c11e18
michaelarita <michael.arita01@gmail.com>
2024-01-11 12:34:03 -0800

    feat: added todo comments for consoleLog and commandExecutor. Finished MVP of commandExecutor.

--- src/events/interactionCreate/commandExecuter.js
+++ src/events/interactionCreate/commandExecuter.js
@@ -3,8 +3,41 @@ const consoleLog = require('../../tests/consoleLog');
 module.exports = async (client, interaction) => {
   if (!interaction.isChatInputCommand()) return;
 
-  const commands = client.commands;
+  const command = interaction.client.commands.get(interaction.commandName);
+  try {
+    if (!command) throw new Error('CommandMissing: Command not found in collection');
 
-  // console.log('Commands Collection: ', commands);
-  consoleLog(__filename, 8, `Commands Collection: ${JSON.stringify(commands, null, '\t')}`);
+    // TODO: Create devOnly Permission
+
+    // TODO: Create testOnly Permission
+
+    if (command.permissionsRequired?.lenght) {
+      for (const permission of command.permissionsRequired) {
+        if (!interaction.member.permission.has(permission)) {
+          interaction.reply({
+            content: 'Not enough permissions.',
+            ephemeral: true,
+          });
+        }
+      }
+    }
+
+    if (command.botPermissions?.length) {
+      for (const permission of command.botPermissions) {
+        const bot = interaction.guild.members.me;
+        if (!bot.permissions.has(permission)) {
+          interaction.reply({
+            content: 'I don\'t have enough permissions.',
+            ephermeral: true,
+          });
+        }
+      }
+    }
+
+    await command.execute(interaction);
+  }
+  catch (error) {
+    console.log(`There was an error running this command: ${error}`);
+  }
+  // consoleLog(__filename, 8, `Commands Collection: ${JSON.stringify(command, null, '\t')}`);
 };
--- src/tests/consoleLog.js
+++ src/tests/consoleLog.js
@@ -1,5 +1,7 @@
 const path = require('path');
 
+// TODO: Change name of file for less confusions
+// TODO: Make use of functionName parameter
 module.exports = (filePath, lineNumber, output, functionName = null) => {
   const fileName = path.basename(filePath);
 
